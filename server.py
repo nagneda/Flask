cbda3f5c80dcd48327595479d2ade60282aec3e8
KimYeongChan <goldfish2679@naver.com>
2022-09-27 12:27:04 +0900

    flask

--- server.py
+++ server.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask,request,redirect
 import random
 
 app=Flask(__name__)
-
+nextid=4
 topics = [ 
     {'id':1, 'title':'python','body':'python is...' },
     {'id':2, 'title':'c++','body':'c++ is...' },
     {'id':3, 'title':'java','body':'java is...' }
  ]
 
-def template(contents,content):
+def template(contents,content,title):
     return f'''
     <!doctype html>
     <html>
     <head>
         <meta charset='utf-8'>
-        <title>  </title>
+        <title>{title} </title>
     </head>
     <body>
         <h1><a href ="/">FLASK</a></h1>
@@ -31,6 +31,11 @@ def template(contents,content):
     </body>
     </html>
     '''
+
+# def gettitle(id):
+#     for topic in topics:
+#         if int(id)==topics['id']:
+#             return topics['title']
 def getcontents():
     string=''
     for topic in topics:    
@@ -39,17 +44,30 @@ def getcontents():
 
 @app.route('/')
 def index():
-    return template(getcontents(),'<h2>welcome to my world</h2>')
+    return template(getcontents(),'<h2>welcome to my world</h2>','Flask')
     
     
-@app.route('/create/')
+@app.route('/create/', methods=['GET','POST'])
 def create():
-    content='''
-    <p><input type = "text" name="title" placeholder="Enter the title..."></p>
-    <p><textarea name="body" placeholder="Enter the sentence..."></textarea></p>
-    <input type = "submit" value="submit">
-    '''
-    return template(getcontents(),content)
+    if request.method == 'GET':
+        content='''
+            <form action="/create/" method="POST">
+                <p><input type = "text" name="title" placeholder="Enter the title..."></p>
+                <p><textarea name="body" placeholder="Enter the sentence..."></textarea></p>
+                <input type = "submit" value="submit">
+            </form>
+        '''
+        return template(getcontents(),content,'Create page')
+    elif request.method == 'POST':
+        global nextid
+        title=request.form['title']
+        body=request.form['body']
+        newtopic={'id':nextid,'title':title,'body':body}
+        topics.append(newtopic)
+        url = '/read/'+str(nextid)+'/'
+        nextid+=1
+        return redirect(url)#redirect 사용자의 웹브라우저에게 어디로 이동하라고 명령가능
+    
 
 
     
@@ -57,12 +75,12 @@ def create():
 def read(id):
     
     for topic in topics:
-        if int(id)==topic['id']:
+        if int(id)==topic['id']:#기본적으로 id값은 숫자긴 하지만 위의 route를 통해 문자열로 인식됐기 때문에 int로 변환하는 과정이 필요
             body=topic['body']
             title=topic['title']
             break
             
-    return template(getcontents(), f'<h2>{title}</h2>{body}')
+    return template(getcontents(), f'<h2>{title}</h2>{body}',title)
     
 app.run(debug=True)
 
