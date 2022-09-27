8bb0791053a994b3072b4625faf29372f56ef299
KimYeongChan <goldfish2679@naver.com>
2022-09-27 14:13:20 +0900

    flask

--- server.py
+++ server.py
@@ -9,7 +9,12 @@ topics = [
     {'id':3, 'title':'java','body':'java is...' }
  ]
 
-def template(contents,content,title):
+def template(contents,content,title,id=None):
+    update=''
+    if id!=None:
+        update=f'''
+        <li><a href = "/update/{id}/">update</a></li>
+        '''
     return f'''
     <!doctype html>
     <html>
@@ -27,6 +32,7 @@ def template(contents,content,title):
         <ul>
 
             <li><h2><a href="/create/">create</a></h2></li>
+            {update}
         </ul>
     </body>
     </html>
@@ -45,7 +51,35 @@ def getcontents():
 @app.route('/')
 def index():
     return template(getcontents(),'<h2>welcome to my world</h2>','Flask')
-    
+
+@app.route('/update/<int:id>/', methods=['GET','POST'])
+def update(id):
+    if request.method == 'GET':
+        for topic in topics:
+            if id==topic['id']:
+                body=topic['body']
+                title=topic['title']
+                break
+        content=f'''
+            <form action="/update/{id}" method="POST">
+                <p><input type = "text" name="title" value="{title}"></p>
+                <p><textarea name="body" >{body}</textarea></p>
+                <input type = "submit" value="update">
+            </form>
+        '''
+        return template(getcontents(),content,'Update page')
+    elif request.method == 'POST':
+        
+        title=request.form['title']
+        body=request.form['body']
+        for topic in topics:
+            if topic['id']==id:
+                topic['title']=title
+                topic['body']=body
+                break
+        url = '/read/'+str(id)+'/' #read라우트로 이동하며 topic에 새로운 id에 해당하는 title,body가 모두 저장돼있고 웹페이지도 구현할 수 있는상태
+        return redirect(url)#redirect 사용자의 웹브라우저에게 어디로 이동하라고 명령가능
+
     
 @app.route('/create/', methods=['GET','POST'])
 def create():
@@ -62,13 +96,11 @@ def create():
         global nextid
         title=request.form['title']
         body=request.form['body']
-        newtopic={'id':nextid,'title':title,'body':body}
+        newtopic={'id':nextid,'title':title,'body':body}#새로 sumbit할때마다 title과 body(text박스안에)를 가진 딕셔너리를 topic에 추가.
         topics.append(newtopic)
-        url = '/read/'+str(nextid)+'/'
+        url = '/read/'+str(nextid)+'/' #read라우트로 이동하며 topic에 새로운 id에 해당하는 title,body가 모두 저장돼있고 웹페이지도 구현할 수 있는상태
         nextid+=1
         return redirect(url)#redirect 사용자의 웹브라우저에게 어디로 이동하라고 명령가능
-    
-
 
     
 @app.route('/read/<id>/')
@@ -80,7 +112,7 @@ def read(id):
             title=topic['title']
             break
             
-    return template(getcontents(), f'<h2>{title}</h2>{body}',title)
+    return template(getcontents(), f'<h2>{title}</h2>{body}',title,id)
     
 app.run(debug=True)
 
